9a7f7cd86c682d86c9e32098b22540feb2d34bb1
Anand Rago <49811947+MoviesLiker@users.noreply.github.com>
2024-02-15 15:33:23 +0530

    Update settings.py

--- manage/settings.py
+++ manage/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'root',
-        'PASSWORD': 'Bigstone@123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
